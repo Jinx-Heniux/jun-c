0f11f18fdbe709d9a547324f1d5997ff203f0f85
Zhang Shaojun (CI/OSX1) <zhs2si@bosch.com>
2022-10-21 15:39:14 +0200

    first commit

---
+++ basics/main.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+int main() {
+	int i1 = 3;
+	i1 = 5;
+	printf("i1: %d\n", i1);
+
+	printf("string: Hello World!\n");
+
+	float f1 = 3e-3;
+	printf("f1: %f\n", f1);
+
+	char c1 = 'a';
+	c1 = c1 - 32;
+	printf("c1: %c\n", c1);
+
+	if ('\0' == 0) {
+		printf("True\n");
+	}
+	else {
+		printf("False\n");
+	}
+
+	int i2 = 5;
+	float f2 = i2 / 2;
+	float f3 = (float)i2 / 2;
+	printf("f2: %f\n", f2);
+	printf("f3: %f\n", f3);
+
+	return 0;
+}
---
+++ scanf/main.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int i1;
+	printf("Input an integer: ");
+	scanf("%d", &i1);
+	printf("i1: %d\n", i1);
+
+	/*char c1;
+	printf("Input a character: ");
+	scanf("%c", &c1);
+	printf("c1: %c", c1);*/
+
+	int i2;
+	printf("Input an integer: ");
+	scanf("%d", &i2);
+	printf("i2: %d\n", i2);
+
+	float f1;
+	printf("Input a float number: ");
+	scanf("%f", &f1);
+	printf("f1: %f\n", f1);
+
+}
