9c74130a010cbf5f8f4bfe0de16a33d37cb12b03
Jinx-Heniux <jinx.heniux.2019@gmail.com>
2022-11-01 13:13:09 +0100

    scanf while if

--- scanf/main.c
+++ scanf/main.c
@@ -1,14 +1,23 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 void test1();
-void runnian();
+void runnian1();
+void whiletest1();
+void whiletest2();
+void iftest1();
 
 int main()
 {
-	/*test1();*/
+	// test1();
 
-	runnian();
+	// runnian1();
+
+	// whiletest1();
+
+	// whiletest2();
+
+	iftest1();
 }
 
 void test1() {
@@ -33,7 +42,7 @@ void test1() {
 	printf("f1: %f\n", f1);
 }
 
-void runnian() {
+void runnian1() {
 	int year;
 	scanf("%d", &year);
 	if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
@@ -41,5 +50,36 @@ void runnian() {
 	}
 	else {
 		printf("no");
+	}
+}
+
+void whiletest1() {
+	int i;
+	// printf("%d", i); // 错误，uninitialized local variable 'i' used
+	scanf("%d", &i);
+	printf("i=%d\n", i);
+	while (i != 0) {
+		scanf("%d", &i);
+		printf("i=%d\n", i);
+	}
+}
+
+void whiletest2() {
+	int i,re;
+	while (rewind(stdin), (re=scanf("%d", &i)) != EOF) { // while (scanf("%d", &i))
+		printf("re=%d, i=%d\n", re, i);
+	}
+}
+
+void iftest1() {
+	char c;
+	while (scanf("%c", &c) != EOF) {
+		if (c != '\n') {
+			printf("%c", c - 32);
+		}
+		else {
+			printf("\n");
+		}
+		
 	}
 }
