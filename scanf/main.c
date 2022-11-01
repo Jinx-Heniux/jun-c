a3b69048b8acbd570002de9b49580490a3957bae
Jinx-Heniux <jinx.heniux.2019@gmail.com>
2022-11-01 12:36:13 +0100

    scanf

--- scanf/main.c
+++ scanf/main.c
@@ -1,17 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+void test1();
+void runnian();
+
 int main()
 {
+	/*test1();*/
+
+	runnian();
+}
+
+void test1() {
 	int i1;
 	printf("Input an integer: ");
 	scanf("%d", &i1);
 	printf("i1: %d\n", i1);
 
-	/*char c1;
+	char c1;
 	printf("Input a character: ");
 	scanf("%c", &c1);
-	printf("c1: %c", c1);*/
+	printf("c1: %c", c1);
 
 	int i2;
 	printf("Input an integer: ");
@@ -22,5 +31,15 @@ int main()
 	printf("Input a float number: ");
 	scanf("%f", &f1);
 	printf("f1: %f\n", f1);
+}
 
+void runnian() {
+	int year;
+	scanf("%d", &year);
+	if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+		printf("yes");
+	}
+	else {
+		printf("no");
+	}
 }
